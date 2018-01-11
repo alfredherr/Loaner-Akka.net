9201b8fbc050ae22111b050d0ab165eb43d49fe8
Alfredo Herrera <alfredherr@gmail.com>
2018-01-10 22:13:39 -0700

    linux env variables

--- Loaner/tools/setEnvironmentVariables.sh
+++ Loaner/tools/setEnvironmentVariables.sh
@@ -1,8 +1,12 @@
 
+export BUSINESS_RULES_FILENAME=business_rules_map.rules
+export COMMANDS_TO_RULES_FILENAME=commands_to_rules.rules
 export StatsDServer=localhost
 export StatsDPort=8125
 export StatsDPrefix=akka-demo
 
+echo $BUSINESS_RULES_FILENAME
+echo $COMMANDS_TO_RULES_FILENAME
 echo $StatsDServer
 echo $StatsDPort
 echo $StatsDPrefix
