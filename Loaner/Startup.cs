05d2aa358df4a130d46b60c87a9b76001dae6d3f
Alfredo Herrera <alfredherr@gmail.com>
2018-01-16 03:05:28 -0700

    moved HOCON config file externally

--- Loaner/Startup.cs
+++ Loaner/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace Loaner
+﻿using System.IO;
+
+namespace Loaner
 {
     using BoundedContexts.MaintenanceBilling.Aggregates.Messages;
     using System;
@@ -160,98 +162,7 @@
 
         private static Config GetConfiguration()
         {
-            var hocon = @" 
-              
-               akka 
-               {
-                    actor 
-                    {
-                     serializers 
-                        {
-                          hyperion = ""Akka.Serialization.HyperionSerializer, Akka.Serialization.Hyperion"" 
-                        }
-                        serialization-bindings 
-                        {
-                            ""System.Object"" = hyperion
-                        }
-                    }
-                }            
-                 
-                akka.actor.debug.lifecycle = on
-                akka.actor.debug.unhandled = on
-                
-
-                akka.loglevel = DEBUG
-                
-                akka.loggers=[""Akka.Logger.NLog.NLogLogger, Akka.Logger.NLog""]
-                     
-                ### PostgreSQL Journal ###
-                #akka.persistence.journal.plugin = ""akka.persistence.journal.postgresql""
-                #akka.persistence.journal.postgresql.class = ""Akka.Persistence.PostgreSql.Journal.PostgreSqlJournal, Akka.Persistence.PostgreSql""
-                #akka.persistence.journal.postgresql.plugin-dispatcher = ""akka.actor.default-dispatcher""
-                #akka.persistence.journal.postgresql.connection-string = ""Server=127.0.0.1;Port=5432;Database=akka;User Id=akka;Password=Testing123;""
-                #akka.persistence.journal.postgresql.connection-timeout = 30s
-                #akka.persistence.journal.postgresql.schema-name = public
-                #akka.persistence.journal.postgresql.table-name = event_journal  
-                #akka.persistence.journal.postgresql.auto-initialize = on
-                #akka.persistence.journal.postgresql.timestamp-provider = ""Akka.Persistence.Sql.Common.Journal.DefaultTimestampProvider, Akka.Persistence.Sql.Common""
-                #akka.persistence.journal.postgresql.metadata-table-name = metadata
-                ## defines column db type used to store payload. Available option: BYTEA (default), JSON, JSONB
-                #akka.persistence.journal.postgresql.stored-as = BYTEA
-                
-                ### SqLite Journal ###
-                #akka.persistence.journal.plugin = ""akka.persistence.journal.sqlite""
-                #akka.persistence.journal.sqlite.class = ""Akka.Persistence.Sqlite.Journal.SqliteJournal, Akka.Persistence.Sqlite""
-                #akka.persistence.journal.sqlite.plugin-dispatcher = ""akka.actor.default-dispatcher""
-                #akka.persistence.journal.sqlite.connection-timeout = 30s
-                #akka.persistence.journal.sqlite.table-name = event_journal
-                #akka.persistence.journal.sqlite.metadata-table-name = journal_metadata
-                #akka.persistence.journal.sqlite.auto-initialize = on
-                #akka.persistence.journal.sqlite.timestamp-provider = ""Akka.Persistence.Sql.Common.Journal.DefaultTimestampProvider, Akka.Persistence.Sql.Common""
-                #akka.persistence.journal.sqlite.connection-string = ""Data Source=../../../akka_demo.db""
-                
-                ### SqLite Snapshot ###
-                #akka.persistence.snapshot-store.plugin = ""akka.persistence.snapshot-store.sqlite""
-                #akka.persistence.snapshot-store.sqlite.class = ""Akka.Persistence.Sqlite.Snapshot.SqliteSnapshotStore, Akka.Persistence.Sqlite""
-                #akka.persistence.snapshot-store.sqlite.plugin-dispatcher = ""akka.actor.default-dispatcher""
-                #akka.persistence.snapshot-store.sqlite.connection-timeout = 30s
-                #akka.persistence.snapshot-store.sqlite.table-name = snapshot_store
-                #akka.persistence.snapshot-store.sqlite.auto-initialize = on
-                #akka.persistence.snapshot-store.sqlite.connection-string = ""Data Source=../../../akka_demo.db""
-
-                ### Jonfile Snapshot ###
-		        akka.persistence.snapshot-store.jonfile.class = ""SnapShotStore.FileSnapshotStore3, SnapShotStore""
-                akka.persistence.snapshot-store.jonfile.max-load-attempts=19
-                akka.persistence.snapshot-store.jonfile.dir = ""C:\\temp""
-                akka.persistence.snapshot-store.jonfile.plugin-dispatcher = ""akka.actor.default-dispatcher""
-                akka.persistence.snapshot-store.plugin = ""akka.persistence.snapshot-store.jonfile""
-
-                ### Kafka Config ####
-                Akka.NumAccountPublisherActor = 10 
-                Akka.NumPortfolioPublisherActor = 10 
-                Akka.AccountStateTopicName = ""jontest103"" 
-                Kafka.BrokerList = ""docker01.concordservicing.com:29092,docker02.concordservicing.com:29092,docker03.concordservicing.com:29092""
-
-                ### StatsD Config ##
-                Akka.StatsDServer = ""docker04""
-                Akka.StatsDPort   = 8125  
-                Akka.StatsDPrefix = ""akka-demo"" 
-
-                ### Business Rules Config ###
-                Akka.BusinessRulesFilename = ""C:\\Temp\\business_rules_map.rules""
-                Akka.CommandsToRulesFilename = ""C:\\Temp\\commands_to_rules.rules""
-
-
-                ### Akka Clustering Config ###
-                akka.actor.provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
-                akka.remote.log-remote-lifecycle-events = INFO
-                akka.remote.dot-netty.tcp.hostname = ""127.0.0.1""
-                akka.remote.dot-netty.tcp.port = 0
-                akka.cluster.seed-nodes = [""akka.tcp://demoSystem@127.0.0.1:4053""] 
-                akka.cluster.roles = [concord]
-
-
-           ";
+            var hocon = File.ReadAllText(Directory.GetCurrentDirectory() + "/Configuration/HOCONConfiguration.hocon");
             var conf = ConfigurationFactory.ParseString(hocon);
             return conf;
         }
