1e2e6d3bb32693b7b07e379ada2fa58e3e198047
Alfredo Herrera <alfredherr@gmail.com>
2018-02-28 15:15:49 -0700

    updated sample data generator

--- Loaner/SampleData/GenerateSampleData.pl
+++ Loaner/SampleData/GenerateSampleData.pl
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings FATAL => 'all';
-die "You must provide the number of records to create. \n usage: GenerateSampleData.pl <#_of_Records> <#_of_Records_Per_Portfolio>" if @ARGV != 2;
+die "You must provide the number of records to create. \n usage: GenerateSampleData.pl <Total Number of Records> <First Portfolio Size>" if @ARGV != 2;
 my $NumberOfRecords = $ARGV[0];
 my $RecordsPerPortfolio = $ARGV[1];
 
@@ -13,8 +13,6 @@ my $obligationCounter = 1;
 my @Chars = ('1'..'9');
 my $Length = 11;
 my %unique=();
-my $portfolio = "ABC";
-print STDOUT "The first portfolio is: $portfolio\n";
 my $portfolio_splitter = 0;
 my $account_number = 10000000000;
 my @inventory = (
@@ -28,27 +26,248 @@ my @inventory = (
     );
 my @daysDelinquent = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,135,145,123,120);
 my @delinquentAmount = (-20,-50,-400,-23);
-
+my @portfolioNames = (
+'JacksonHole',
+'Rose',
+'PuebloBonito',
+'GranRegina',
+'WildernessLodge',
+'JamboHouse',
+'KidaniVillage',
+'BeachClubVillas',
+'BoardWalkVillas',
+'Anaheim',
+'AngelFire',
+'AngelFireCabinShareI',
+'AngelsCamp',
+'AsiaPacificSurfersParadise',
+'AsiaPacificTorquay',
+'Austin',
+'AvenuePlaza',
+'BaliHaiVillas',
+'BayClubII',
+'BayVoyageInn',
+'BeachStreetCottages',
+'BentleyBrook',
+'BisonRanch',
+'BonnetCreek',
+'BostonBeaconHill',
+'BransonatTheFalls',
+'BransonatTheMeadows',
+'CanterburyatSanFrancisco',
+'ClearwaterBeach',
+'CypressPalms',
+'DesertBlue',
+'Durango',
+'DyeVillasatMyrtleBeach',
+'EmeraldGrandeatDestin',
+'Flagstaff',
+'FozdoIguau',
+'Galena',
+'GovernorsGreen',
+'GrandChicagoRiverfront',
+'GrandDesert',
+'GrandLake',
+'GrandPittsburghDowntown',
+'GreatSmokiesLodge',
+'HarbourLights',
+'Indio',
+'InnonLongWharf',
+'InnontheHarbor',
+'KaEoKai',
+'KauaiBeachVillas',
+'KingCottonVillas',
+'Kingsgate',
+'KonaHawaiian',
+'LaBelleMaison',
+'LaCascada',
+'LakeMarion',
+'LakeoftheOzarks',
+'LongWharf',
+'MaunaLoaVillage',
+'MidtownatNewYorkCity',
+'MountainVista',
+'Nashville',
+'NewportOnshore',
+'NewportOverlook',
+'OceanBoulevard',
+'OceanRidge',
+'OceanWalk',
+'OceansidePier',
+'OldTownAlexandria',
+'Pagosa',
+'PalmAire',
+'PanamaCityBeach',
+'ParkCity',
+'PatriotsPlace',
+'Pinetop',
+'PratagyBeach',
+'RamadaCoffsHarbourTreetops',
+'RamadaGoldenBeach',
+'RamadaWanaka',
+'RamadaevenMileBeach',
+'RanchoVistoso',
+'atAvon',
+'atFairfieldBay',
+'atFairfieldGlade',
+'atFairfieldMountains',
+'atFairfieldSapphireValley',
+'ReunionatOrlando',
+'RioMar',
+'RiversideSuites',
+'RoyalGardenatWaikiki',
+'RoyalSeaCliff',
+'RoyalVista',
+'SantaBarbara',
+'SeaGardens',
+'SeaWatchPlantation',
+'Sedona',
+'ShawneeVillageCrestview',
+'ShawneeVillageDepuy',
+'ShawneeVillageFairwayVillage',
+'ShawneeVillageRidgeTop',
+'ShawneeVillageRiverVillageI',
+'ShawneeVillageRiverVillageII',
+'Shearwater',
+'SkylineTower',
+'SmokyMountains',
+'SmugglersNotchVermont',
+'SouthShore',
+'StThomas',
+'StarIsland',
+'StarrPassGolfSuites',
+'SteamboatSprings',
+'SundaraCottagesatWisconsinDells',
+'SydneySuites',
+'Tamarack',
+'Taos',
+'TheDonatello',
+'TheLegacyGolf',
+'TheMillsHouseGrandHotel',
+'TheQueenMaryHotel',
+'TowersontheGroveatNorthMyrtleBeach',
+'TropicanaatLasVegas',
+'VinoBello',
+'VintageLandingatFourSeasons',
+'Westwinds',
+'WorldMarkAnaheim',
+'WorldMarkAnaheimDolphinsCove',
+'WorldMarkAngelsCamp',
+'WorldMarkArrowPoint',
+'WorldMarkBassLake',
+'WorldMarkBearLake',
+'WorldMarkBendSeventhMountain',
+'WorldMarkBigBear',
+'WorldMarkBirchBay',
+'WorldMarkBisonRanch',
+'WorldMarkBlaine',
+'WorldMarkBranson',
+'WorldMarkCanmoreBanff',
+'WorldMarkCathedralCity',
+'WorldMarkChelanLakeHouse',
+'WorldMarkClearLake',
+'WorldMarkCoralBaja',
+'WorldMarkDaytonaBeachOceanWalk',
+'WorldMarkDeerHarbor',
+'WorldMarkDepoeBay',
+'WorldMarkDiscoveryBay',
+'WorldMarkEagleCrest',
+'WorldMarkEstesPark',
+'WorldMarkFiji',
+'WorldMarkFortLauderdalePalmAire',
+'WorldMarkFortLauderdaleSantaBarbara',
+'WorldMarkFortLauderdaleSeaGardens',
+'WorldMarkGalena',
+'WorldMarkGleneden',
+'WorldMarkGranbyRockyMountainPreserve',
+'WorldMarkGrandLake',
+'WorldMarkHavasuDunes',
+'WorldMarkHuntStablewoodSprings',
+'WorldMarkIndio',
+'WorldMarkIslaMujeres',
+'WorldMarkKapaaShore',
+'WorldMarkKihei',
+'WorldMarkKona',
+'WorldMarkLaPaloma',
+'WorldMarkLakeChelanShores',
+'WorldMarkLakeTahoe',
+'WorldMarkLakeoftheOzarks',
+'WorldMarkLasVegasBoulevard',
+'WorldMarkLasVegasSpencerStreet',
+'WorldMarkLasVegasTropicanaAve',
+'WorldMarkLeavenworth',
+'WorldMarkLongBeach',
+'WorldMarkMarbleFalls',
+'WorldMarkMarinaDunes',
+'WorldMarkMarinerVillage',
+'WorldMarkMcCall',
+'WorldMarkMidway',
+'WorldMarkNewBraunfels',
+'WorldMarkNewOrleansAvenuePlaza',
+'WorldMarkOceanside',
+'WorldMarkOrlandoKingstownReef',
+'WorldMarkOrlandoReunion',
+'WorldMarkPagosa',
+'WorldMarkPalmSprings',
+'WorldMarkPalmSpringsPlazapa',
+'WorldMarkParkCity',
+'WorldMarkPhoenixSouthMountainPreserve',
+'WorldMarkPinetop',
+'WorldMarkPismoBeach',
+'WorldMarkRanchoVistoso',
+'WorldMarkRedRiver',
+'WorldMarkReno',
+'WorldMarkRunningY',
+'WorldMarkSanDiegoBalboaPark',
+'WorldMarkSanDiegoInnatthePark',
+'WorldMarkSanDiegoMissionValley',
+'WorldMarkSanFrancisco',
+'WorldMarkSantaFe',
+'WorldMarkSchoonerLanding',
+'WorldMarkScottsdale',
+'WorldMarkSeaside',
+'WorldMarkSeattleTheCamlin',
+'WorldMarkSolvang',
+'WorldMarkSouthPacificClubbyCairns',
+'WorldMarkSouthShore',
+'WorldMarkStGeorge',
+'WorldMarkStThomasElysianBeach',
+'WorldMarkSteamboatSprings',
+'WorldMarkSurfsideInn',
+'WorldMarkTaos',
+'WorldMarkValleyIsle',
+'WorldMarkVancouverTheCanadian',
+'WorldMarkVictoria',
+'WorldMarkWestYellowstone',
+'WorldMarkWhistlerCascadeLodge',
+'WorldMarkWindsor',
+'WorldMarkWolfCreek',
+'WorldMarkZihuatanejo',
+'GlacierCanyon',
+'MajesticSun',
+'NationalHarbor',
+'TheCottages',
+'WaikikiBeachWalk');
+my $portfolio = "VillaDelMar";
+print STDOUT "The first portfolio is: $portfolio\n";
+my @portfolioSizes = (10000,20000,30000,40000,50000,60000,70000,80000);
 for(my $i = 1; $i <= $NumberOfRecords ; $i++ ){
-    my $number = '';
-    #for (1..$Length) {
-    #    $number .= $Chars[int rand @Chars];
-    #}
-    $number = $account_number++;
+    my $number = $account_number++;
 
-     if($portfolio_splitter != 0 &&  $portfolio_splitter % $RecordsPerPortfolio == 0){
-         $portfolio = join('', map +(q(A)..q(Z))[rand(26)], 1..3);
-         print STDOUT "$portfolio_splitter new portfolio: $portfolio\n";
-     }
     if(exists $unique{$number}){
         $unique{$number}++;    
-    }else{
+    }else{	
+     	if($portfolio_splitter != 0 && $portfolio_splitter % $RecordsPerPortfolio == 0){
+        	 $portfolio =  $portfolioNames[rand @portfolioNames];
+        	 print STDOUT "$portfolio_splitter new portfolio: $portfolio\n";
+		$RecordsPerPortfolio = $portfolioSizes[rand @portfolioSizes];
+	}
         my $amountDelinquent = 0;
         my $delinquent = $daysDelinquent[rand @daysDelinquent];
         if($delinquent gt 0){
             $amountDelinquent = $delinquentAmount[rand @delinquentAmount];
         }
-        print CLIENT join("\t","PORTFOLIO$portfolio",$number,"Username".join('', map +(q(A)..q(Z))[rand(26)], 1..3), "246.00",$inventory[ rand @inventory ],$delinquent,$amountDelinquent,"\n");
+        print CLIENT join("\t",$portfolio,$number,"Username".join('', map +(q(A)..q(Z))[rand(26)], 1..3), "246.00",$inventory[ rand @inventory ],$delinquent,$amountDelinquent,"\n");
         print OBLIGATIONS join("\t",$obligationCounter++,$number,"Maintenance","123.00"),"\n";
         print OBLIGATIONS join("\t",$obligationCounter++,$number,"Loan","123.00"),"\n";
         $unique{$number}++;
