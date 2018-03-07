abe450bb53e1f9451c678a96392ca925f7d1c910
Alfredo Herrera <alfredherr@gmail.com>
2018-03-06 23:48:35 -0700

    removed dispatcher; cleaned up local build script

--- devrun.sh
+++ devrun.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml down && cd Loaner/ && docker build -t alfredherr/loaner:1.16 . && cd ../ && docker-compose -f docker-compose.yml up 
+docker-compose -f docker-compose.yml down && cd Loaner/ && docker build -t alfredherr/loaner . && cd ../ && docker-compose -f docker-compose.yml up 
