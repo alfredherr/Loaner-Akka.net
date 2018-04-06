a6d829ab421da040e40a1e3ebe7e90d44b3d8ada
Alfredo Herrera <alfredherr@gmail.com>
2018-04-06 13:59:10 -0700

    quick hack to get correct portfolio name over to kafka

--- Loaner/BoundedContexts/MaintenanceBilling/Aggregates/AccountActor.cs
+++ Loaner/BoundedContexts/MaintenanceBilling/Aggregates/AccountActor.cs
@@ -30,6 +30,9 @@ namespace Loaner.BoundedContexts.MaintenanceBilling.Aggregates
         private AccountState _accountState = new AccountState();
 
 
+        private string ParentName = ""; //hack to  pass porfolio name to kafka upon boarding -
+        //TODO move to portfolio state maybe? Should the account know about its parent in its state? Hmm.
+
         private DateTime _lastBootedOn;
 
         public AccountActor()
@@ -120,11 +123,15 @@ namespace Loaner.BoundedContexts.MaintenanceBilling.Aggregates
 
         private void PublishToKafka(PublishAccountStateToKafka msg)
         {
+
+            
+            ParentName = Self.Path.Parent.Name.Contains("$") ? ParentName : Self.Path.Parent.Name;
+ 
             var kafkaAccountModel = new AccountStateViewModel
             (
                 accountNumber: _accountState.AccountNumber,
                 userName: _accountState.UserName,
-                portfolioName: Self.Path.Parent.Name,
+                portfolioName: ParentName, //Self.Path.Parent.Name,
                 currentBalance: _accountState.CurrentBalance,
                 accountStatus: _accountState.AccountStatus,
                 asOfDate: DateTime.Now,
@@ -216,6 +223,7 @@ namespace Loaner.BoundedContexts.MaintenanceBilling.Aggregates
                 , (double) _accountState.CurrentBalance)
             );
 
+            this.ParentName = command.Portfolio;
             //ReportMyState(0,(double) _accountState.CurrentBalance, command.MyNewParent);
 
             //Report current state to Kafka
