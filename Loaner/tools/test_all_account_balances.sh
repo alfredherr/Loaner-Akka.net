05f3666aa5d0512617f236acc3ddc51c2916da5a
Alfredo Herrera <alfredherr@gmail.com>
2019-08-08 16:35:47 -0700

    Update test_all_account_balances.sh

--- Loaner/tools/test_all_account_balances.sh
+++ Loaner/tools/test_all_account_balances.sh
@@ -1,1 +1,1 @@
-cat Raintree.txt|awk '{print "http://localhost/api/account/"$2"/info" }'|xargs curl -s|jq '.' |grep -i currentbalance|sort |uniq  -c
+cat Greentree.txt|awk '{print "http://localhost/api/account/"$2"/info" }'|xargs curl -s|jq '.' |grep -i currentbalance|sort |uniq  -c
