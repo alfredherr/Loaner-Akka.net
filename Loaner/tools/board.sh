eba2cdcd17d8afcef7adf5c72e6057f5947de058
Alfredo Herrera <alfredherr@gmail.com>
2019-08-08 16:35:03 -0700

    Update board.sh

--- Loaner/tools/board.sh
+++ Loaner/tools/board.sh
@@ -4,7 +4,7 @@ curl -X POST \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: a4c68858-4995-f72e-6e7e-e726aecb42f2' \
   -d '{
-	"ClientName": "Raintree",
-	"ClientAccountsFilePath": "/demo/Raintree.txt",
-	"ObligationsFilePath":"/demo/Obligations/Raintree.txt"
+	"ClientName": "Greentree",
+	"ClientAccountsFilePath": "/demo/Greentree.txt",
+	"ObligationsFilePath":"/demo/Obligations/Greentree.txt"
 }'
